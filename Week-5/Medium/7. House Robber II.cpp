95a4cb1282a4b54fa3cf8fef69d69e7d01d7ac48
shumbul <arifas18922@gmail.com>
2022-09-01 00:21:58 +0530

    medium

--- Week-5/Medium/7. House Robber II.cpp
+++ Week-5/Medium/7. House Robber II.cpp
@@ -1,5 +1,32 @@
 // https://leetcode.com/problems/house-robber-ii/
 
+// Concise
+class Solution {
+    vector<int> rob1(int i, bool first, vector<int>& nums){
+        if((i==nums.size()-1 && first) || i==nums.size()){
+            return {0,0};
+        }
+        auto exclude=rob1(i+1, first, nums);
+        vector<int> res(2, 0);
+        // exclude
+        res[0]=max(exclude[0], exclude[1]);
+        // include
+        res[1]=exclude[0]+nums[i];
+        return res;
+    }
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==1)
+            return nums[0];
+        // robbery starts from house 1
+        vector<int> r1=rob1(0, true, nums);
+        // robbery starts from house 2
+        vector<int> r2=rob1(1, false, nums);
+        return max({r1[0], r1[1], r2[0], r2[1]});
+    }
+};
+
+// easy to understand
 class Solution {
     vector<int> rob1_(int i, vector<int>& nums){
         if(i==nums.size()-1)
