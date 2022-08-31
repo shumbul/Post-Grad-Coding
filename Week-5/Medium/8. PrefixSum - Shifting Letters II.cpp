be340a4323f74b2a72c8cd51b60d3b5a20270aee
shumbul <arifas18922@gmail.com>
2022-09-01 00:59:03 +0530

    readme update

--- Week-5/Medium/8. PrefixSum - Shifting Letters II.cpp
+++ Week-5/Medium/8. PrefixSum - Shifting Letters II.cpp
@@ -1,9 +1,46 @@
 // https://leetcode.com/problems/shifting-letters-ii/
-
+        
+// prefix sum
 class Solution {
 public:
     string shiftingLetters(string S, vector<vector<int>>& shifts) {
-        // brute force
+        vector<int> pre_sum(S.length()+1, 0);
+        for(auto &s: shifts){
+            // populating prefix sum
+            if(s[2]==1)
+                pre_sum[s[0]]++, pre_sum[s[1]+1]--;
+            else
+                pre_sum[s[0]]--, pre_sum[s[1]+1]++;
+            
+            // handling negative here itself
+            if(pre_sum[s[0]]<0)
+                pre_sum[s[0]]=25;
+            if(pre_sum[s[1]+1]<0)
+                pre_sum[s[1]+1]=25;
+        }
+        int sum=0;
+        for(int i=0;i<S.length();i++){
+            // fetching sum based on calculated prefix sum vector
+            sum+=pre_sum[i];
+            S[i]=((S[i]-'a'+sum)%26)+'a';
+        }
+        return S;
+    }
+};
+
+
+/*
+l = length of string s
+n = size of shifts vector
+    Time Complexity: O(max(l, n))
+    Space Complexity: O(l)
+*/
+
+
+// brute force
+class Solution {
+public:
+    string shiftingLetters(string S, vector<vector<int>>& shifts) {
         vector<int> all_shifts(S.length(), 0);
         for(auto &s: shifts){
             // this is time taking
