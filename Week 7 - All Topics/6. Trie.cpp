4be844fde03d32f3795de1f179b6dffeb9bc2627
shumbul <arifas18922@gmail.com>
2022-10-02 02:18:52 +0530

    trie

---
+++ Week 7 - All Topics/6. Trie.cpp
@@ -0,0 +1,55 @@
+// TRIE
+
+class Node {
+    public:
+        string data;
+    unordered_map < string,
+    Node *>children;
+    bool isTerminal;
+
+    Node(string &val) {
+        data=val;
+        isTerminal=false;
+    }
+}
+
+;
+
+class Trie {
+    Node* root;
+
+    public: Trie() {
+        root=new Node("\0");
+    }
+
+    void insert(string &word) {
+        Node* temp=root;
+
+        for (char &ch: word) {
+            if (temp - > children.count(s)==0) {
+                Node * n=new Node(s);
+                temp ->children[s]=n;
+            }
+
+            temp=temp ->children[s];
+        }
+
+        temp ->isTerminal=true;
+    }
+
+    bool search(string &word) {
+        Node* temp=root;
+
+        for (char &ch: word) {
+            if (temp - > children.count(s)==0) {
+                return false;
+            }
+
+            temp=temp ->children[s];
+        }
+
+        temp ->isTerminal=true;
+    }
+}
+
+;
